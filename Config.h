ae625cf8ad8a3fdde070bfe40b63463840e39baa
Saad <33311165+alor-pretatma@users.noreply.github.com>
2020-03-03 23:20:38 +0600

    Add files via upload

---
+++ Config.h
@@ -0,0 +1,5 @@
+const int id=1;
+const char* key="";
+const char* ssid = "TP-Link_ADFC";
+const char* pass = "passwordnai";
+const char* url = "http://192.168.0.120/IrfanVai/api/test.php";
---
+++ FreeRTOS.ino
@@ -0,0 +1,35 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "Config.h";
+
+void setup(){
+  Serial.begin(115200);
+  delay(2000);
+  WiFi.begin(ssid, pass);
+  Serial.print("Connecting.");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Wifi ok!");
+}
+void sendData(void* params){
+  HTTPClient http;
+  Serial.println("Sending data..");
+  http.begin(url);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded"); 
+
+  int httpCode=http.POST("id="+String(id)+"&key="+String(key)+"&temp=100&heart=3.1416&spo2=122&ecg=66");
+  if (httpCode > 0) { //Check for the returning code
+    Serial.println(http.getString());
+  }else {
+    Serial.printf("\nError: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+  Serial.println("A task ended!");
+  vTaskDelete(NULL);
+}
+void loop(){
+  xTaskCreate(sendData, "sendData", 3000, NULL, 1, NULL);
+  delay(2000);
+}
